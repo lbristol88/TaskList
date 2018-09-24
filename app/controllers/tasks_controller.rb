518f7a357ee3a01dae50156a5516eab38d79a0e3
Layla <layla.bristol@gmail.com>
2018-09-23 20:50:53 -0700

    Wave 1 complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find(task_id)
+    if @task.nil?
+      head :not_found
+    end
+  end
+
 end
